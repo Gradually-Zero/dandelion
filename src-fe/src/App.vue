fc74687938be11b863bc2eb9785d8ef7365cfbff
mnz8 <mnz8@foxmail.com>
2024-09-22 03:50:49 +0800

    feat: 调整功能

--- src-fe/src/App.vue
+++ src-fe/src/App.vue
@@ -1,117 +1,37 @@
-<script setup lang="ts">
-import { ref, onMounted } from 'vue'
-import { RouterLink, RouterView } from 'vue-router'
-import { invoke } from '@tauri-apps/api/core'
-import { open } from '@tauri-apps/plugin-dialog'
-import { listen } from '@tauri-apps/api/event'
+<script setup>
+import { ref, watch } from 'vue'
+import { useRoute } from 'vue-router'
 
-// 使用 ref 以便 Vue 能响应式更新
-const selected_file = ref<string>('')
-const jsonAst = ref<string>('')
+const items = [
+  { name: 'home', icon: 'mdi-application-outline', route: '/' },
+  { name: 'find', icon: 'mdi-magnify', route: '/find' },
+  { name: 'conf', icon: 'mdi-cog', route: '/conf' }
+]
 
-// 监听文件变化
-listen<string>('selected-change', (event) => {
-  selected_file.value = event.payload
-})
+const route = useRoute()
+// 初始选中为当前路由的路径
+const selectedItem = ref(route.path)
 
-// 获取文件
-onMounted(async () => {
-  try {
-    selected_file.value = await invoke<string>('get_selected_file')
-  } catch (error) {
-    console.error('Error fetching selected file:', error)
+// 监听路由变化，更新选中状态
+watch(
+  () => route.path,
+  (newPath) => {
+    selectedItem.value = newPath
   }
-})
-
-const parseMd = async () => {
-  try {
-    const temp = await invoke<string>('get_markdown_ast')
-    jsonAst.value = temp
-    console.log('jsonAst', jsonAst)
-    console.log('MDAST:', JSON.parse(temp))
-  } catch (error) {
-    console.log('parseMd', error)
-  }
-}
-
-const selectFile = async () => {
-  try {
-    const selectedPath = await open({
-      filters: [{ name: 'Markdown 文件', extensions: ['md'] }],
-      multiple: false
-    })
-
-    if (selectedPath) {
-      await invoke('set_selected_file', { filePath: selectedPath })
-    }
-  } catch (error) {
-    console.error('选择文件失败:', error)
-  }
-}
+)
 </script>
 
 <template>
-  <header>
-    <nav>
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/write">Write</RouterLink>
-      <RouterLink to="/table">Table</RouterLink>
-    </nav>
-  </header>
-  <RouterView />
-  <Suspense>
-    <template #default>
-      <div>选择的文件：{{ selected_file }}</div>
-    </template>
-    <template #fallback>
-      <div>加载中...</div>
-    </template>
-  </Suspense>
-  <v-btn @click="selectFile"> 选择文件 </v-btn>
-  <v-btn @click="parseMd"> 解析 </v-btn>
-  <div>{{ jsonAst }}</div>
   <v-app>
-    <v-bottom-navigation>
-      <v-btn value="recent">
-        <v-icon>mdi-table-large</v-icon>
-      </v-btn>
-      <v-btn value="favorites">
-        <v-icon>mdi-application-outline</v-icon>
-      </v-btn>
-      <v-btn value="conf">
-        <v-icon>mdi-cog</v-icon>
+    <v-bottom-navigation v-model="selectedItem" app>
+      <v-btn v-for="item in items" :key="item.name" :to="item.route" icon>
+        <v-icon>{{ item.icon }}</v-icon>
       </v-btn>
     </v-bottom-navigation>
+    <v-main>
+      <v-container>
+        <router-view />
+      </v-container>
+    </v-main>
   </v-app>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-</style>
