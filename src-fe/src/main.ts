2d66f96075fad94e6757e8def2b5b39d3edfa2d4
mnz8 <mnz8@foxmail.com>
2024-09-23 02:20:13 +0800

    feat: 更换 ui 库。简要功能

--- src-fe/src/main.ts
+++ src-fe/src/main.ts
@@ -1,14 +1,7 @@
 import './assets/main.css'
-import '@mdi/font/css/materialdesignicons.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
-// Vuetify
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-
-const vuetify = createVuetify({})
 
 import App from './App.vue'
 import router from './router'
@@ -17,6 +10,5 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-app.use(vuetify)
 
 app.mount('#app')
--- src-fe/src/views/Find.vue
+++ src-fe/src/views/Find.vue
@@ -1,21 +1,52 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
 import { invoke } from '@tauri-apps/api/core'
+import { getFirstTableData } from '@/utilities'
 
-const jsonAst = ref<string>('')
+const tableData = ref<Record<string, string | undefined>[]>([])
+const errorMessage = ref<string>('')
+const searchQuery = ref<string>('')
+
 const parseMd = async () => {
   try {
+    errorMessage.value = ''
     const temp = await invoke<string>('get_markdown_ast')
-    jsonAst.value = temp
-    console.log('jsonAst', jsonAst)
-    console.log('MDAST:', JSON.parse(temp))
+    const ast: MdastNode = JSON.parse(temp)
+    const firstTableData = getFirstTableData(ast)
+    tableData.value = firstTableData
   } catch (error) {
-    console.log('parseMd', error)
+    if (error instanceof Error) {
+      errorMessage.value = '解析失败: ' + error.message
+    } else {
+      errorMessage.value = '解析失败: 未知错误'
+    }
+    console.log('parseMd error:', error)
   }
 }
+
+const columns = [
+  { title: '1', dataKey: 'cell-0', width: 150 },
+  { title: '2', dataKey: 'cell-1', width: 150 },
+  { title: '3', dataKey: 'cell-2', width: 150 },
+  { title: '4', dataKey: 'cell-3' }
+]
+
+onMounted(() => {
+  parseMd()
+})
 </script>
 
 <template>
-  <v-btn @click="parseMd"> 解析 </v-btn>
-  <div>{{ jsonAst }}</div>
+  <el-space style="margin-bottom: 8px">
+    <el-input />
+    <el-button @click="parseMd" type="primary">解析</el-button>
+  </el-space>
+  <div style="height: 80vh; width: 80vw">
+    <el-auto-resizer>
+      <template #default="{ height, width }">
+        <el-table-v2 :columns="columns" :data="tableData" :width="width" :height="height" />
+      </template>
+    </el-auto-resizer>
+  </div>
+  <div v-if="errorMessage">{{ errorMessage }}</div>
 </template>
