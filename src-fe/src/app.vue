d51387c789f837fb68ad27c239a227d6a43deca9
mnz8 <mnz8@foxmail.com>
2026-05-03 03:16:27 +0800

    feat: 更换图标

--- src-fe/src/app.vue
+++ src-fe/src/app.vue
@@ -2,16 +2,8 @@
 import { computed, onBeforeUnmount, onMounted, ref, watch } from 'vue'
 import { useRoute } from 'vue-router'
 import { getCurrentWindow } from '@tauri-apps/api/window'
-import {
-  CloseBold,
-  CopyDocument,
-  EditPen,
-  Files,
-  House,
-  Minus,
-  Search,
-  Setting
-} from '@element-plus/icons-vue'
+import { FileCode, House, Minus, Search, Settings, Square, X } from '@lucide/vue'
+import WindowRestoreIcon from '@/components/WindowRestoreIcon.vue'
 
 const route = useRoute()
 const appWindow = getCurrentWindow()
@@ -87,7 +79,7 @@ onBeforeUnmount(() => {
           aria-label="最小化"
           @click="minimizeWindow"
         >
-          <el-icon><Minus /></el-icon>
+          <Minus class="app-titlebar__icon app-titlebar__icon-lg" :size="18" />
         </button>
         <button
           class="app-titlebar__button"
@@ -95,10 +87,8 @@ onBeforeUnmount(() => {
           :aria-label="isMaximized ? '还原窗口' : '最大化窗口'"
           @click="toggleWindowMaximize"
         >
-          <el-icon>
-            <CopyDocument v-if="isMaximized" />
-            <Files v-else />
-          </el-icon>
+          <WindowRestoreIcon v-if="isMaximized" class="app-titlebar__icon" :size="16" />
+          <Square v-else class="app-titlebar__icon" :size="16" />
         </button>
         <button
           class="app-titlebar__button app-titlebar__button--danger"
@@ -106,7 +96,7 @@ onBeforeUnmount(() => {
           aria-label="关闭窗口"
           @click="closeWindow"
         >
-          <el-icon><CloseBold /></el-icon>
+          <X class="app-titlebar__icon app-titlebar__icon-lg" :size="18" />
         </button>
       </div>
     </header>
@@ -115,19 +105,19 @@ onBeforeUnmount(() => {
       <el-aside class="app-layout__aside" width="200px">
         <el-menu :default-active="current" :router="true" style="border-right: 0">
           <el-menu-item index="/">
-            <el-icon><House /></el-icon>
+            <House class="app-menu__icon" />
             <span>Home</span>
           </el-menu-item>
           <el-menu-item index="/find">
-            <el-icon><Search /></el-icon>
+            <Search class="app-menu__icon" />
             <span>Find</span>
           </el-menu-item>
           <el-menu-item index="/editor">
-            <el-icon><EditPen /></el-icon>
+            <FileCode class="app-menu__icon" />
             <span>Editor</span>
           </el-menu-item>
           <el-menu-item index="/conf">
-            <el-icon><Setting /></el-icon>
+            <Settings class="app-menu__icon" />
             <span>Configuration</span>
           </el-menu-item>
         </el-menu>
@@ -208,6 +198,15 @@ onBeforeUnmount(() => {
     color 0.2s ease;
 }
 
+.app-titlebar__icon-lg {
+  width: 18px;
+  height: 18px;
+}
+
+.app-titlebar__icon {
+  display: block;
+}
+
 .app-titlebar__button:hover {
   background: color-mix(in srgb, var(--el-fill-color-light) 86%, var(--el-color-primary) 14%);
 }
@@ -224,6 +223,16 @@ onBeforeUnmount(() => {
 
 .app-layout__aside {
   border-right: 1px solid var(--el-menu-border-color);
+}
+
+.app-menu__icon {
+  width: 1em;
+  height: 1em;
+  margin-right: 8px;
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  flex-shrink: 0;
 }
 
 .app-layout__main {
---
+++ src-fe/src/components/WindowRestoreIcon.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import { Copy } from '@lucide/vue'
+</script>
+
+<template>
+  <Copy style="transform: scaleX(-1)" />
+</template>
